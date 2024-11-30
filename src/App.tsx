d50f72c298e38b0c998c74b46ff7304909aab07e
Antonio <tothantonio000@gmail.com>
2024-11-30 13:20:03 +0200

    completed the tile context

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { PathFindingProvider } from "./context/PathFindingContext";
+import { TileProvider } from "./context/TileContext";
 
 function App() {
   return (
     <PathFindingProvider>
-    <h1 className="text-3xl font-bold underline h-screen w-screen bg-blue-500">
-      Hello world!
-    </h1>
+      <TileProvider>
+        <h1 className="text-3xl font-bold underline h-screen w-screen bg-blue-500">
+          Hello world!
+        </h1>
+      </TileProvider>
     </PathFindingProvider>
   );
 }
---
+++ src/context/TileContext.tsx
@@ -0,0 +1,32 @@
+import { createContext, useState } from "react";
+import { TileType } from "../utils/types";
+import { START_TILE_CONFIG, END_TILE_CONFIG } from "../utils/constants";
+
+interface TileContextInterface {
+  startTile: TileType;
+  setStartTile: (startTile: TileType) => void;
+  endTile: TileType;
+  setEndTile: (endTile: TileType) => void;
+}
+
+export const TileContext = createContext<TileContextInterface | undefined>(
+  undefined
+);
+
+export const TileProvider = ({ children }: { children: React.ReactNode }) => {
+  const [startTile, setStartTile] = useState<TileType>(START_TILE_CONFIG);
+  const [endTile, setEndTile] = useState<TileType>(END_TILE_CONFIG);
+
+  return (
+    <TileContext.Provider
+      value={{
+        startTile,
+        setStartTile,
+        endTile,
+        setEndTile,
+      }}
+    >
+      {children}
+    </TileContext.Provider>
+  );
+};
